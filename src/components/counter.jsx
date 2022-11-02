d72f9b8623f71ee283da8cd32502ddf4f5c70f54
RezaSoltani <59668273+reza6969@users.noreply.github.com>
2022-11-03 02:19:23 +0330

    Components
    :-1:
    4- Specifying Children
    5- Embedding Expressions
    6- Setting Attributes
    7- Rendering Classes Dynamically
    8 - Rendering Lists
    9- Conditional Rendering
    10- Handling Events
    11- Binding Event Handlers
    12- Updating the State
    14- Passing Event Arguments
    :+1:

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -9,8 +9,44 @@ class Counter extends Component {
     // };
     state = {
         count: 0,
+        // imageUrl: 'https://picsum.photos/200'
+        tags: ['tag1', 'tag2', 'tag3']
     };
+
+    styles = {
+        fontSize: 10,
+        fontWeight: 'bold'
+    };
+
+    // constructor() {
+    //     super();
+    //     this.handleIncrement = this.handleIncrement.bind(this);
+    // }
+
+    renderTags() {
+        if(this.state.tags.length === 0) return <p>There are no tags!</p>;
+
+        return <ul>{ this.state.tags.map((tag) => <li key={tag}>{tag}</li>)}</ul>;
+    }
+
+    // handleIncrement() {
+    //     console.log('Increment Clicked!', this.state.count);
+    // }
+
+    handleIncrement = product => {
+        // console.log('Increment Clicked!', this.state.count);
+        // this.state.count++;
+        console.log(product);
+        this.setState({ count: this.state.count + 1});
+    }
+
+    // doHandleIncrement = () => {
+    //     this.handleIncrement({ id: 1});
+    // }
+
+
     render() {
+        // let classes = this.getBadgeClasses();
         // return <><h1>Hello World!!</h1><button>Increment</button></>;
         // React.createElement("div");
         // return <div><h1>Hello World!!</h1><button>Increment</button><div>;
@@ -21,11 +57,25 @@ class Counter extends Component {
             // </div>
             <React.Fragment>
                 {/* <h1>Hello World!!</h1> */}
+                {/* <img src={this.state.imageUrl} alt="" /> */}
                 {/* <span>{this.state.count}</span> */}
-                <span>{this.formatCount()}</span>
-                <button>Increment</button>
+                {/* <span style={ this.styles } className='badge bg-primary m-2'>{this.formatCount()}</span> */}
+                {/* <span style={ this.styles } className={classes}>{this.formatCount()}</span> */}
+                <span style={ this.styles } className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                {/* <button onClick={this.handleIncrement} className='btn btn-secondary btn-sm'>Increment</button> */}
+                {/* <button onClick={this.doHandleIncrement} className='btn btn-secondary btn-sm'>Increment</button> */}
+                <button onClick={ () => this.handleIncrement({ id: 1})} className='btn btn-secondary btn-sm'>Increment</button>
+                {/* <ul>{ this.state.tags.map((tag) => <li key={tag}>{tag}</li>)}</ul> */}
+                { this.state.tags.length === 0 && "Please create a new tag!"}
+                { this.renderTags() }
             </React.Fragment>
         );
+    }
+
+    getBadgeClasses() {
+        let classes = 'badge m-2 bg-';
+        classes += (this.state.count === 0) ? 'warning' : 'primary';
+        return classes;
     }
 
     formatCount() {
